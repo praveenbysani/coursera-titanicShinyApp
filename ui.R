68af7e8dd45055749383bdc606dcc2f6d07d7d14
praveenbysani <praveen.iiith@gmail.com>
2014-06-22 21:54:18 +0800

    help notes added

--- ui.R
+++ ui.R
@@ -4,17 +4,22 @@ shinyUI(fluidPage(
   # populate side bar layout
   sidebarLayout(
     #two input widgets, for gender and age inputs
+    
     sidebarPanel(
-      radioButtons("gender","Select whether survived or deceased",
+     
+      #p(This webapp enables the user to explore the effects of factors like travel calss, gender and age group on the survival probability.),
+      
+      radioButtons("gender","Select the Gender class of the passenger",
                    c("Male"="Male","Female"="Female"),selected="Male"),
       selectInput("age","Select the age group of passengers",c("Child"="Child","Adult"="Adult"),
-                  selected="Adult")
-      ),
+                  selected="Adult"),
+      helpText("Note:: BY changing the gender and age group, one can observe the changes in survival probability plot")
+    ),
     #main panel to plot the result
     mainPanel(
       plotOutput("plot1",height="500px")
-      )
     )
-  
   )
-      )
+  
+)
+)
